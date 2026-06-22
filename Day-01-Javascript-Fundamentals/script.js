9ced2581f556408586e59fd843c097b3b43d02fe
Mspatil06 <mallikarjunpatil399@gmail.com>
2026-06-22 18:45:33 +0530

    Day 01 - JavaScript Fundamentals

---
+++ Day-01-Javascript-Fundamentals/script.js
@@ -0,0 +1,69 @@
+
+// VARIABLES
+
+let studentName = "Mallikarjun"
+const college = "Engineering College"
+var age = 22
+console.log("Student Name:", studentName)
+console.log("College:", college)
+console.log("Age:", age)
+
+// DATA TYPES
+
+let fullName = "Mallikarjun"
+let studentAge = 22
+let isPlaced = false
+let salary = null
+let futureGoal
+console.log(typeof fullName)
+console.log(typeof studentAge)
+console.log(typeof isPlaced)
+console.log(typeof salary)
+console.log(typeof futureGoal)
+
+// OPERATORS
+
+let num1 = 20
+let num2 = 10
+console.log("Addition:", num1 + num2)
+console.log("Subtraction:", num1 - num2)
+console.log("Multiplication:", num1 * num2)
+console.log("Division:", num1 / num2)
+console.log("Modulus:", num1 % num2)
+
+// INPUT / OUTPUT
+
+let userName = prompt("Enter your name")
+console.log("Welcome", userName)
+alert("Hello " + userName)
+
+
+// COMMENTS
+
+let firstNumber = 15
+let secondNumber = 25
+let sum = firstNumber + secondNumber
+console.log("Sum =", sum)
+
+
+let width = 5
+let area = length * width
+console.log("Area of Rectangle =", area)
+
+
+let celsius = 30
+let fahrenheit = (celsius * 9/5) + 32
+console.log("Temperature in Fahrenheit =", fahrenheit)
+
+
+let a = 5
+let b = 10
+console.log("Before Swap")
+console.log("a =", a)
+console.log("b =", b)
+let temp = a
+a = b
+b = temp
+console.log("After Swap")
+console.log("a =", a)
+console.log("b =", b)
